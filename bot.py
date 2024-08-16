32e00da2e412ccc4d1b2ae2bb56360aa2dbd7453
F1NH4WK <eduardo.ar33@gmail.com>
2024-08-16 17:21:55 -0300

    feat: vemai command

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler,MessageHandler, filters, ContextTypes
 import asyncio
+import datetime
 
 # Carregar o nosso TOKEN - boa prática
 dotenv_path = join(dirname(__file__), '.env')
@@ -13,13 +14,26 @@ load_dotenv(dotenv_path)
 TOKEN = os.environ.get("TOKEN")
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def acende(update: Update, context: ContextTypes.DEFAULT_TYPE):
+	# COLOCAR A RANDOMIZAÇÃO DE PRA PRA PRA
 	name = update.message.from_user.first_name
 	await update.message.reply_text(f"Olá, eu sou um bot! Como posso te ajudar, {name}?")
 
 async def teste(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 	await update.message.reply_text("Comando recebido com sucesso!")
 	
+async def vemai(update: Update, context: ContextTypes.DEFAULT_TYPE):
+	month = datetime.datetime.now().month
+	msg = ""
+
+	if 1 < month <= 5:
+		msg = "TOC TOC, É O PANCADASI BATENDO NA TUA PORTA!!! TEM QUE SER MALUCO PRA PERDER!!!"
+
+	if 5 < month <= 9:
+		msg = "EU OUVI MELHOR FESTA DO ANO? ISSO MESMO, VEM AÍ A GLOW CARALHO!"
+
+	await update.message.reply_text(msg)
+
 application = None
 
 async def initialize_application():
@@ -27,7 +41,8 @@ async def initialize_application():
 	
 	if application is None:
 		application = ApplicationBuilder().token(TOKEN).build()
-		application.add_handler(CommandHandler("start", start))
+		application.add_handler(CommandHandler("start", acende))
+		application.add_handler(CommandHandler("vemai", vemai))
 		application.add_handler(MessageHandler(filters.COMMAND, teste))
 		await application.initialize()
 
