33a65bae78a34e986479ea78cc466edeb2dfa319
F1NH4WK <eduardo.ar33@gmail.com>
2024-08-22 16:16:45 -0300

    feat: s3 integration and contagem command

--- bot.py
+++ bot.py
@@ -2,29 +2,56 @@ import json
 import os
 import logging
 from os.path import join, dirname
-from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler,MessageHandler, filters, ContextTypes
 import asyncio
 import datetime
+import boto3
+from typing import Dict
+from dotenv import load_dotenv
 
 # Carregar o nosso TOKEN - boa prática
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 TOKEN = os.environ.get("TOKEN")
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-async def acende(update: Update, context: ContextTypes.DEFAULT_TYPE):
-	# COLOCAR A RANDOMIZAÇÃO DE PRA PRA PRA
-	await update.message.reply_text("PRA PRA PRA  PRA POW POW")
-	await update.message.reply_text("pra")
-	await update.message.reply_text("pra pra")
-	await update.message.reply_text("pra pra pra")
-	await update.message.reply_text("pow")
-	await update.message.reply_text("pow pow")
+#Init S3
+s3 = boto3.client(
+	's3',
+	aws_access_key_id = AWS_ACCESS_KEY_ID,
+	aws_secret_access_key = AWS_SECRET_ACCESS_KEY
+)
 
-	
-async def vemai(update: Update, context: ContextTypes.DEFAULT_TYPE):
+BUCKET_NAME = "contagem-rojao"
+KEY = "contagem_rojao.json"
+
+def _getContagem() -> Dict:
+	response = s3.get_object(Bucket = BUCKET_NAME, Key = KEY) 
+	return json.load(response['Body'])
+
+def _putContagem() -> bool:
+	cont = _getContagem()
+	s3.put_object(Bucket = BUCKET_NAME, Key = KEY, Body = json.dumps({'contagem': cont['contagem'] + 1}))
+	return True
+
+#s3.put_object(Bucket = BUCKET_NAME, Key = KEY, Body = json.dumps({'contagem': 4033}))
+
+async def acende(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+	# COLOCAR A RANDOMIZAÇÃO DE PRA PRA PRA
+
+	_putContagem()
+
+	await update.message.reply_text("PRA PRA PRA  PRA POW POW", quote = False)
+	await update.message.reply_text("pra", quote = False)
+	await update.message.reply_text("pra pra", quote = False)
+	await update.message.reply_text("pra pra pra", quote = False)
+	await update.message.reply_text("pow", quote = False)
+	await update.message.reply_text("pow pow", quote = False)
+
+async def vemai(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 	month = datetime.datetime.now().month
 	msg = ""
 
@@ -39,6 +66,12 @@ async def vemai(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 	await update.message.reply_text(msg)
 
+async def contagem(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+	cont = _getContagem()
+	print(cont['contagem'])
+	await update.message.reply_text(f"O DASI Já usou o Rojão DASIANO {cont['contagem']} vezes")
+
+
 application = None
 
 async def initialize_application():
@@ -48,6 +81,7 @@ async def initialize_application():
 		application = ApplicationBuilder().token(TOKEN).build()
 		application.add_handler(CommandHandler("acende", acende))
 		application.add_handler(CommandHandler("vemai", vemai))
+		application.add_handler(CommandHandler("contagem", contagem))
 		await application.initialize()
 
 async def handle_request(event):
@@ -62,7 +96,7 @@ async def handle_request(event):
 		logging.error(f"Error processing update: {e}")
 		raise
         
-def lambda_handler(event, context):
+def lambda_handler(event, context) -> dict:
 	asyncio.run(initialize_application())
 	asyncio.run(handle_request(event))
 
